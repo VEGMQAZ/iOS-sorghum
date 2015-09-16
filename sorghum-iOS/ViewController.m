5a5e019eea1d22c3f919f5dc5b3ab446958e92dc
michaelbradshaw <mjbradshaw87@gmail.com>
2015-09-16 11:05:31 -0500

    -added elements to data input screen -added segue to data input screen

--- sorghum-iOS/ViewController.m
+++ sorghum-iOS/ViewController.m
@@ -14,14 +14,29 @@
 
 @implementation ViewController
 
-- (void)viewDidLoad {
+- (void)viewDidLoad
+{
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
 }
 
-- (void)didReceiveMemoryWarning {
+- (void)didReceiveMemoryWarning
+{
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
+}
+
+- (void)loginSuccess
+{
+    [self performSegueWithIdentifier:@"LoginSegue" sender:self];
+}
+
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender
+{
+    if ([segue.identifier isEqualToString:@"LoginSegue"]) {
+        // Declare a pointer to the destination controller
+        // set up properties
+    }
 }
 
 @end
